34d1a36c15a9e3ad345d44f6d690ca8c1bd6e399
sidheequ <sidheequj@gmail.com>
2025-12-19 16:23:11 +0400

    fix(build): final removal of imageUrls import and trigger redeploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
+// Final build fix for image URL resolution
 import '../index.css';
 
 const Navbar = () => {
